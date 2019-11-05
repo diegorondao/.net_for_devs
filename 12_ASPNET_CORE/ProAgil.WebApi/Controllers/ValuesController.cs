f777b93bb606425ef15787e08434a6ee33563329
Diego Rondão <drondao@autodoc.com.br>
2019-11-05 10:55:03 -0300

    Add projeto .NetCore WebApi

---
+++ 12_ASPNET_CORE/ProAgil.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAgil.WebApi.Data;
+using ProAgil.WebApi.Model;
+
+namespace ProAgil.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dbContext;
+
+        public ValuesController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Evento>> Get()
+        {
+            return _dbContext.Eventos.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Evento> Get(int id)
+        {
+           return _dbContext.Eventos.FirstOrDefault(x => x.EventoId == id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
